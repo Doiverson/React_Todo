3c20217a871c433962f9829d153cc517cdb53013
ShosukeDoi <doiverson.canada@gmail.com>
2020-08-29 17:43:59 -0700

    added delete function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,10 +11,14 @@ class Todo extends React.Component {
         const newArr  = this.state.list;
         newArr.push(this.state.input);
         this.setState({ list: newArr, input: '' })
-
     }
 
     onHandleChange = (e) => this.setState({input: e.target.value})
+
+    onDeleteClick = (i) => {
+        const newArr = this.state.list.filter((item, index) => i !== index);
+        this.setState({list: newArr})
+    }
 
     render() {
         return (
@@ -22,7 +26,12 @@ class Todo extends React.Component {
                <input value={this.state.input} onChange={this.onHandleChange}/>
                <button onClick={this.onHandleClick}>ADD</button>
                 <ul>
-                    {this.state.list.map((item, index) => <li key={index}>{item}</li>)}
+                    {this.state.list.map((item, index) =>
+                        <div>
+                            <li style={{display: "inline-block", marginRight: 10}} key={index}>{item}</li>
+                            <button onClick={() => this.onDeleteClick(index)}>Delete</button>
+                        </div>
+                    )}
                 </ul>
             </div>
         );
